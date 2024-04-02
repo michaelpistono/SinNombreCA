02a0822e34a55e32f1012a24a2a033758bae78e3
Michael Pistono <114769413+michaelpistono@users.noreply.github.com>
2024-04-02 10:21:58 -0700

    Update app.R

--- app.R
+++ app.R
@@ -1,13 +1,18 @@
 
-
 library(shiny)
 library(tidyverse)
 library(readr)
 library(leaflet)
 library(sf)
+library(patchwork)
+library(reshape2)
+library(car)
+library(magrittr)
 
 
-# UI definition
+
+
+# Define UI for application
 ui <- fluidPage(
   titlePanel("California Hantavirus Dashboard"),
   
@@ -19,23 +24,24 @@ ui <- fluidPage(
     mainPanel(
       fluidRow(
         column(
-          width = 12,  # Adjust the width as needed
+          width = 12,  
           tabsetPanel(
-            tabPanel("Vector Seropositivity Map",
-                     leafletOutput("vector_prevalence_map")), 
-            tabPanel("Human Cases Map",
-                     leafletOutput("hantavirus_cases_map")),            
-            tabPanel("Human Cases by County",
-                     plotOutput("human_cases_plot")),
-            tabPanel("Vector Seropositivity by County",
-                     plotOutput("hantavirus_carriage_plot")),
-            tabPanel("Linear Model",
-                     plotOutput("lm_plot"))
+            tabPanel("Vector Prevalence",
+                     fluidRow(
+                       column(width = 12, leafletOutput("vector_prevalence_map")),
+                       column(width = 12, plotOutput("hantavirus_carriage_plot"))
+                     )),
+            tabPanel("Human Cases",
+                     fluidRow(
+                       column(width = 12, leafletOutput("hantavirus_case_map")),
+                       column(width = 12, plotOutput("human_cases_plot"))
+                     )),
+            tabPanel("Human Prevalence",
+                     fluidRow(
+                       column(width = 12, leafletOutput("human_prevalence_map")),
+                       column(width = 12, plotOutput("human_prevalence_plot"))
+                     ))
           )
-        ),
-        column(
-          width = 11, 
-          
         )
       )
     )
@@ -48,7 +54,7 @@ server <- function(input, output, session) {
   
   
   output$sidebar_text <- renderText({
-"Hantaviruses are vector-borne pathogens
+    "Hantaviruses are vector-borne pathogens
 that cause severe respiratory disease 
 in humans. Sin Nombre hantavirus--the
 most common in the United States--is
@@ -81,73 +87,70 @@ homes looking for food or nesting material.
 Infestations are also common in sheds, 
 abandoned vehicles and seasonal cabins 
 that lay dormant for long periods."
+    
   })
   
   data <- reactive({
-    # Read and preprocess data
+    
     Hantavirus_update <- read_csv('Deer Mouse Prevalence.csv')
-    counties <- read_csv('Countypopulations.csv')
     
-    # Merge data
-    Hantavirus_updated <- merge(Hantavirus_update, counties, by = "County")
-    
-    # Data manipulation
-    Hantavirus_updated <- Hantavirus_updated %>%
-      mutate(pPrev = Prevalence,
-             Human_pPrev = Human_Cases / Human_Population,
-             prevalence_ratio = Human_pPrev / (Prevalence/100))
-    
-    Hantavirus_updated
+    Hantavirus_updated <- Hantavirus_update %>%
+      mutate(human_prevalence = human_cases / human_population,
+             vector_prevalence = avg_vector_prevalence/100,
+             prevalence_ratio = human_prevalence/avg_vector_prevalence,
+             pop_density = (1/pop_density))
   })
   
-  # Render vector prevalence map
+  
   output$vector_prevalence_map <- renderLeaflet({
     req(data())
     
-    # Load California county boundaries
-    county_boundaries <- st_read("california_counties.dbf") 
     
-    # Merge vector prevalence with county boundaries
-    county_map_data <- merge(county_boundaries, data(), by.x = "NAME", by.y = "NAME", all.x = TRUE)
-    # Filter out missing values from the "Prevalence" column
-    county_map_data <- county_map_data[!is.na(county_map_data$Prevalence), ]
+    county_boundaries <- st_read("california_counties.shp") 
     
-    # Create a color palette for the legend
-    pal <- colorNumeric(palette = "YlOrRd", domain = county_map_data$Prevalence, na.color = "white")
+ 
+    county_map_data <- merge(county_boundaries, data(), by.x = "GEOIDFQ", by.y = "AFFGEOID", all.x = TRUE)
     
-    # Create the Leaflet map
+    county_map_data <- county_map_data[!is.na(county_map_data$avg_vector_prevalence), ]
+    
+    
+    pal <- colorNumeric(palette = "YlOrRd", domain = county_map_data$avg_vector_prevalence, na.color = "white")
+    
+  
     leaflet(county_map_data) %>%
-      # Remove the basemap
+     
       setView(lng = -119.4179, lat = 36.7783, zoom = 5.3) %>% # Adjust the view as needed
       addTiles() %>%
-      # Add polygons with custom fillColor
-      addPolygons(fillColor = ~pal(Prevalence),
+      
+      addPolygons(fillColor = ~pal(avg_vector_prevalence),
                   fillOpacity = 1,
                   weight = 1,
                   color = "black",
-                  popup = ~paste(County, "<br>", "Prevalence: ", Prevalence)) %>%
-      # Add legend
+                  popup = ~paste(County, "<br>", "Prevalence: ", avg_vector_prevalence)) %>%
+      
       addLegend(position = "bottomright", # Position the legend
                 pal = pal, # Specify the color palette
-                values = ~Prevalence, # Values to label in the legend
+                values = ~avg_vector_prevalence, # Values to label in the legend
                 title = "% Seropositive Mice", # Title of the legend
                 opacity = 1) # Adjust legend opacity if needed
+    
+    
   })
   
   # Render Hantavirus cases map
-  output$hantavirus_cases_map <- renderLeaflet({
+  output$hantavirus_case_map <- renderLeaflet({
     req(data())
     
     # Load California county boundaries
-    county_boundaries <- st_read("california_counties.dbf") 
+    county_boundaries <- st_read("california_counties.shp") 
     
     # Merge case counts with county boundaries
-    county_map_data <- merge(county_boundaries, data(), by.x = "NAME", by.y = "NAME", all.x = TRUE)
-    # Filter out missing values from the "Human_Cases" column
-    county_map_data <- county_map_data[!is.na(county_map_data$Human_Cases), ]
+    county_map_data <- merge(county_boundaries, data(), by.x = "GEOIDFQ", by.y = "AFFGEOID", all.x = FALSE)
+   
+    county_map_data <- county_map_data[!is.na(county_map_data$human_cases), ]
     
     # Create a color palette for the legend
-    pal <- colorNumeric(palette = "YlOrRd", domain = county_map_data$Human_Cases, na.color = "white")
+    pal <- colorNumeric(palette = "YlOrRd", domain = county_map_data$human_cases, na.color = "white")
     
     # Create the Leaflet map
     leaflet(county_map_data) %>%
@@ -155,27 +158,87 @@ that lay dormant for long periods."
       setView(lng = -119.4179, lat = 36.7783, zoom = 5.3) %>% # Adjust the view as needed
       addTiles() %>%
       # Add polygons with custom fillColor
-      addPolygons(fillColor = ~pal(Human_Cases),
+      addPolygons(fillColor = ~pal(human_cases),
                   fillOpacity = 1,
                   weight = 1,
                   color = "black",
-                  popup = ~paste(County, "<br>", "Cases: ", Human_Cases)) %>%
+                  popup = ~paste(County, "<br>", "Prevalence: ", human_cases)) %>%
       # Add legend
       addLegend(position = "bottomright", # Position the legend
                 pal = pal, # Specify the color palette
-                values = ~Human_Cases, # Values to label in the legend
+                values = ~human_cases, # Values to label in the legend
                 title = "Human Cases", # Title of the legend
                 opacity = 1) # Adjust legend opacity if needed
   })
+  
+
+  output$human_prevalence_map <- renderLeaflet({
+    req(data())
+    
+    # Load California county boundaries
+    county_boundaries <- st_read("california_counties.shp") 
+    
+    # Merge case counts with county boundaries
+    county_map_data <- merge(county_boundaries, data(), by.x = "GEOIDFQ", by.y = "AFFGEOID", all.x = FALSE)
+    
+    county_map_data <- county_map_data[!is.na(county_map_data$human_cases), ]
+    county_map_data <- county_map_data%>%
+      mutate(human_prevalence = (human_cases / human_population)*10000,
+             vector_prevalence = avg_vector_prevalence/100,
+             prevalence_ratio = human_prevalence/avg_vector_prevalence,
+             pop_density = (1/pop_density))
+    
+    # Create a color palette for the legend
+    pal <- colorNumeric(palette = "YlOrRd", domain = county_map_data$human_prevalence, na.color = "white")
+    
+    # Create the Leaflet map
+    leaflet(county_map_data) %>%
+      # Remove the basemap
+      setView(lng = -119.4179, lat = 36.7783, zoom = 5.3) %>% # Adjust the view as needed
+      addTiles() %>%
+      # Add polygons with custom fillColor
+      addPolygons(fillColor = ~pal(human_prevalence),
+                  fillOpacity = 1,
+                  weight = 1,
+                  color = "black",
+                  popup = ~paste(County, "<br>", "Prevalence: ", human_prevalence)) %>%
+      # Add legend
+      addLegend(position = "bottomright", # Position the legend
+                pal = pal, # Specify the color palette
+                values = ~human_prevalence, # Values to label in the legend
+                title = "Cases/10,000 People", # Title of the legend
+                opacity = 1) # Adjust legend opacity if needed
+  })
+  
+  
+  
+  output$hantavirus_carriage_plot <- renderPlot({
+    req(data())
+    data() %>%
+      filter(avg_vector_prevalence > 0) %>%
+      group_by(County) %>%
+      ggplot(aes(x = reorder(County, avg_vector_prevalence), y = avg_vector_prevalence)) +
+      geom_bar(stat = "identity", fillColor = ~pal(avg_vector_prevalence)) +
+      theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust = 1)) +
+      theme_test()+
+      coord_flip() +
+      scale_y_continuous(limits = c(0, 40)) +
+      labs(title = "Average Hantavirus Prevalence Among Deer Mice, 2000 - 2022",
+           x = "County",
+           y = "Prevalence (%)")
+  })
+  
+  
+  
   
   # Render human cases plot
   output$human_cases_plot <- renderPlot({
     req(data())
     data() %>%
-      filter(Human_Cases > 0) %>%
+      filter(human_cases > 0) %>%
       group_by(County) %>%
-      ggplot(aes(x = reorder(County, Human_Cases), y = Human_Cases)) +
-      geom_bar(stat = "identity", fill = "black") +
+      ggplot(aes(x = reorder(County, human_cases), y = human_cases)) +
+      geom_bar(stat = "identity", fillColor = ~pal(human_cases)) +
       theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust = 1)) +
       theme_test()+
       coord_flip() +
@@ -185,40 +248,26 @@ that lay dormant for long periods."
            y = "Case Count")
   })
   
+
+  
   # Render hantavirus carriage plot
-  output$hantavirus_carriage_plot <- renderPlot({
+  output$human_prevalence_plot <- renderPlot({
     req(data())
     data() %>%
-      filter(pPrev > 0) %>%
+      filter(human_prevalence > 0) %>%
       group_by(County) %>%
-      ggplot(aes(x = reorder(County, pPrev), y = pPrev)) +
-      geom_bar(stat = "identity", fill = "black") +
+      ggplot(aes(x = reorder(County, human_prevalence), y = human_prevalence)) +
+      geom_bar(stat = "identity", fillColor = ~pal(human_prevalence)) +
       theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust = 1)) +
       theme_test()+
       coord_flip() +
-      scale_y_continuous(limits = c(0, 40)) +
-      labs(title = "Hantavirus Carriage among North American Deer Mice, 2013 - 2022",
+      scale_y_continuous(limits = c(0, .0020)) +
+      labs(title = "Hantavirus Prevalence in Humans, 1980 - 2022",
            x = "County",
-           y = "% Prevalence")
+           y = "Prevalence (%)")
   })
   
-  # Render linear model plot
-  output$lm_plot <- renderPlot({
-    req(data())
-    Updated_lm <- data() %>%
-      filter(Human_Cases >= 2)
-    
-    updated_model <- lm(Human_pPrev ~ pPrev, data = Updated_lm)
-    ggplot(Updated_lm, aes(x = pPrev, y = Human_pPrev)) +
-      geom_point() +
-      geom_abline(intercept = -8.824761e-05, slope = 3.191788e-05) +
-      theme_test()+
-      labs(title = "Human Prevalence vs Vector Prevalence (R^2 = .54)",
-           x = "Hantavirus Prevalence in North American Deer Mice (%)",
-           y = "Percent Prevalence in Humans (%)")
-  })
-
 }
 
-# Run
+# Run the application
 shinyApp(ui, server)
